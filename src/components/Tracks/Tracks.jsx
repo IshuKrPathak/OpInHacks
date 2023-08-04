f19642c76d8225957d644d9359f88fbfe98c3abf
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2023-08-04 15:17:00 +0530

    tracks updated

--- src/components/Tracks/Tracks.jsx
+++ src/components/Tracks/Tracks.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect,useCallback } from 'react';
 import "./Tracks.css";
-import opinco from "../../assets/Hackreboot.png";
+// import opinco from "../../assets/Hackreboot.png";
 import sidestairs2 from "../../assets/sidestairs2.png"
 import greenmonster from "../../assets/greenmonster.png"
-import staircubes from "../../assets/staircubes.png"
-import sidestairs from "../../assets/sidestairs.png"
-import img1 from "../../assets/Rectangle.png"
-import first from '../../assets/first.png';
-import first2 from '../../assets/surprise.png'
-import space from "../../assets/space-lines.png"
+// import staircubes from "../../assets/staircubes.png"
+// import sidestairs from "../../assets/sidestairs.png"
+// import img1 from "../../assets/Rectangle.png"
+// import first from '../../assets/first.png';
+// import first2 from '../../assets/surprise.png'
+// import space from "../../assets/space-lines.png"
 import pre from "../../assets/pre.png"
 import next from "../../assets/nxt.png"
 
@@ -27,8 +27,12 @@ const track4 = require('../../assets/Rectangle.png')
 // };
 const images = { track0, track1, track2, track3, track4 }
 
+
+
+
 export const Tracks = () => {
   const [imageSrc, setImageSrc] = useState(images.track0)
+  const [trackNum, setTrackNum] = useState(0)
 
   const changeImage = (newImageSrc) => {
     setImageSrc(newImageSrc);
@@ -38,6 +42,30 @@ export const Tracks = () => {
   const handlemusicprogressbar = (e) => {
     setaudioprogress(e.target.value)
   }
+
+  const changeTrackStyle = useCallback(()=>{
+    const tracks = Array.from(document.getElementsByClassName('track'));
+    tracks.forEach((track)=>{
+      if(track.classList.contains(`track${trackNum}`)){
+        track.classList.add('track-selected');
+      }
+      else{
+        track.classList.remove('track-selected')
+      }
+    })
+  },[trackNum])
+
+  const playNextTrack = (e)=> {
+    e.preventDefault()
+    console.log(trackNum)
+    setTrackNum((prev)=> prev-1 );
+  }
+
+  useEffect(()=>{
+    setImageSrc(images[`track${trackNum}`])
+    changeTrackStyle();
+  },[trackNum,changeTrackStyle])
+
   return (
 
     // <section id="Tracks" className="track-conatainer">
@@ -81,65 +109,101 @@ export const Tracks = () => {
       <h1 className="Theme-text">THEMES</h1>
       {/* <img src={staircubes} alt="" className="img1 greenmonster" /> */}
 
-<img src={sidestairs2} alt="" className="img1 sidestairs2" />
-{/* <img src={space} alt="" className="img1 staircubes" /> */}
-<img src={greenmonster} alt="" className="img1 sidestairs" /> 
-      <div className="left-container">
-        <button className="track1" onTouchMove={() => changeImage(images.track1)}>surprise1</button>
-        <button className="track1" onClick={() => changeImage(images.track2)}>surprise 2</button>
-        <button className="track1" onClick={() => changeImage(images.track3)}>surprise 3</button>
-        <button className="track1" onClick={() => changeImage(images.track4)}>surprise 4</button>
-        <button className="track1" onClick={() => changeImage(images.track3)}>surprise 1</button>
-        <button className="track1" onClick={() => changeImage(images.track4)}>surprise 2</button>
+      <img src={sidestairs2} alt="" className="img1 sidestairs2" />
+      {/* <img src={space} alt="" className="img1 staircubes" /> */}
+      <img src={greenmonster} alt="" className="img1 sidestairs" /> 
+      <div className="track-row">
+        <div className="left-container">
+          <button className="track track0" onClick={(e) => {
+            changeImage(images.track0);
+            setTrackNum(0)
+            changeTrackStyle();
+            }}>
+              Surprise 1
+          </button>
+          <button className="track track1" onClick={(e) => {
+            changeImage(images.track2);
+            setTrackNum(1)
+            changeTrackStyle();
+            }}>
+              Surprise 2
+          </button>
+          <button className="track track2" onClick={(e) => {
+            changeImage(images.track3);
+            changeTrackStyle();
+            setTrackNum(2)
 
-
-      </div>
-      <div className='right-container'>
-
-        <div className="music-card">
-          <img src={imageSrc} alt="GDSC" className="music-img" />
-
-          <div className="music-card-text">
-            <div className="text-music">
-              <p className="theme-text-p">Theme name</p>
-              <p className="theme-org">Theme</p>
-              {/* <div className="musictimerdiv">
-                <div className='musiccurrenttime'>00.00</div>
-                <div className="musictotlalength">04.00</div>
-              </div> */}
-              {/* <div className='seekbar'>
-              <input type="range" name='musicprogressbar' className=' ' value={audioprogress} onChange={handlemusicprogressbar}/>
-
-              </div> */}
-
-
-              <div className="musiccontrolers">
-                <i className='fa-solid fa-backward'></i>
-                <i className='fa-solid fa-circle-play'></i>
-                <i className='fa-solid fa-forward'></i>
-              </div>
-            </div>
-          </div>
-          <div className='song-slider'>
-            <input type="range" value={audioprogress} className='seek-bar' onChange={handlemusicprogressbar
-            } />
-            <span className='current-time'> 00:00</span>
-            <span className='song-duration-time'> 04:00</span>
-          </div>
-
-          <div className='controls'>
-            <button className='btn backward-btn'><img src={pre} alt="" /></button>
-            <button className='play-btn pause'>
-              <span></span>
-              <span></span>
-            </button>
-            <button className='btn forward-btn'><img src={next} alt="" /></button>
-
-          </div>
+            }}>
+              Surprise 3
+          </button>
+          <button className="track track3" onClick={(e) => {
+            changeImage(images.track4);
+            changeTrackStyle();
+            setTrackNum(3)
+            }}>
+              Surprise 4
+          </button>
+          <button className="track track4" onClick={(e) => {
+            changeImage(images.track2);
+            changeTrackStyle();
+            setTrackNum(4)
+            }}>
+              Surprise 5
+          </button>
+            
         </div>
 
 
+        <div className='right-container'>
+          <div className="music-card ">
+            <img src={imageSrc} alt="GDSC" className="music-img" />
+            <div className="music-card-text">
+              <div className="text-music">
+                <p className="theme-text-p">Theme name</p>
+                <p className="theme-org">Theme</p>
+
+                {/* <div className="musictimerdiv">
+                  <div className='musiccurrenttime'>00.00</div>
+                  <div className="musictotlalength">04.00</div>
+                </div> */}
+                {/* <div className='seekbar'>
+                <input type="range" name='musicprogressbar' className=' ' value={audioprogress} onChange={handlemusicprogressbar}/>
+
+                </div> */}
+
+                <div className="musiccontrolers">
+                  <i className='fa-solid fa-backward'></i>
+                  <i className='fa-solid fa-circle-play'></i>
+                  <i className='fa-solid fa-forward'></i>
+                </div>
+              </div>
+            </div>
+            <div className='song-slider'>
+              <input type="range" value={audioprogress} className='seek-bar' onChange={handlemusicprogressbar
+              } />
+              <span className='current-time'> 00:00</span>
+              <span className='song-duration-time'> 04:00</span>
+            </div>
+
+            <div className='controls'>
+              <button className='btn-control backward-btn' onClick={playNextTrack}
+                disabled={trackNum===0 ? true : false}><img src={pre} alt="" /></button>
+              <button className='play-btn pause'>
+                <span></span>
+                <span></span>
+              </button>
+              <button className='btn-control forward-btn' onClick={(e)=> {
+                e.preventDefault()
+                console.log(trackNum)
+                setTrackNum((prev)=> prev+1);
+                setImageSrc(images[`track${trackNum}`])
+              }} disabled={trackNum===4 ? true : false}><img src={next} alt="" /></button>
+
+            </div>
+          </div>
+        </div>
       </div>
+
 
 
 
